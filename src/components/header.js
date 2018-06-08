89cd1bbf372a9e0811d5f9ee8db1d5b70a5c40f3
kadenjenson <sobeit99@gmail.com>
2018-06-08 16:55:47 -0600

    trying a more dynamic approach

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Mad Libs</h1>
+      <p>Fill in the blanks below and then hit the generate button to see your Mad Lib!</p>
+    </div>
+  )
+}
+
+export default Header;
