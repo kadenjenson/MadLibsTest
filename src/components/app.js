6314db653598e5778a7b49fae7f5cce1a27f711f
kadenjenson <sobeit99@gmail.com>
2018-06-08 08:25:57 -0600

    Styling header

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div className="background">
+          <div className="header">
+            <h1>MadLibs</h1>
+          </div>
+          <div className="sub-header">
+            Fill out the fields below and click the generate button to see your Mad Lib.
+          </div>
+        </div>
       </div>
     );
   }
