32dfe1ebce17daf87bfcaa0cf18f21903564eec3
kadenjenson <sobeit99@gmail.com>
2018-06-11 16:59:31 -0600

    Added component for card and input

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from './header';
+import Card from './card';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         { Header('Kadens Mad Libs', 'Fill in the blanks below and then hit the generate button to see your Mad Lib story!') }
+        <Card />
       </div>
     );
   }
