5f540f587fe998d3f42f9bc579ae3feced7ccaa5
kadenjenson <sobeit99@gmail.com>
2018-06-15 16:22:12 -0600

    Added a couple more inputs

--- src/components/card.js
+++ src/components/card.js
@@ -8,7 +8,9 @@ class Card extends Component {
     super()
     this.state = {
       color: '',
-      pluralNoun: ''
+      pluralNoun: '',
+      adjective: '',
+      noun: ''
     }
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -24,6 +26,10 @@ class Card extends Component {
         { Input('Color', this.state.color, this.handleInputChange, 'color') }
         <h1>{this.state.pluralNoun}</h1>
         { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
+        <h1>{this.state.adjective}</h1>
+        { Input('Adjective', this.state.adjective, this.handleInputChange, 'adjective') }
+        <h1>{this.state.noun}</h1>
+        { Input('Noun', this.state.noun, this.handleInputChange, 'noun') }
       </div>
     )
   }
