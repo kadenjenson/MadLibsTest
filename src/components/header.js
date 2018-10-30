ee00e22d3825d34cd155c027f95ea54ebbbaf493
kadenjenson <sobeit99@gmail.com>
2018-10-30 16:14:08 -0600

    started styles for the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
+      <div className="header__skew"></div>
       <h1>Kadens Mad Libs</h1>
       <p>Fill in the blanks below and then hit the generate button to see your Mad Lib story!</p>
     </div>
