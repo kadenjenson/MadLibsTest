6ef98419224320c3502a4032e4d4e447fd62b3d0
kadenjenson <sobeit99@gmail.com>
2018-10-31 13:50:39 -0600

    finished styles for skewed header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__skew"></div>
-      <h1>Kadens Mad Libs</h1>
+      <h1>Kado Libs</h1>
       <p>Fill in the blanks below and then hit the generate button to see your Mad Lib story!</p>
     </div>
   )
